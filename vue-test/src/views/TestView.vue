cb59fe106aeb2b042ea6500366bd9430921a91ab
jwdouble <954649468@qq.com>
2022-06-05 20:52:02 +0800

    1

---
+++ vue-test/src/views/TestView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        测试
+        <!-- <router-link to="http://150.158.7.96:32056/health">
+           axios
+        </router-link> -->
+        <button @click="TestAxios">axios</button>
+    </div>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+export default{
+    name:"Test",
+    components:{
+
+    },
+    methods:{
+        TestAxios(){
+            axios.get("http://150.158.7.96:32056/health").then(function(data){
+                console.log(data);
+            }).catch(function(data){
+                console.log(data);
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
